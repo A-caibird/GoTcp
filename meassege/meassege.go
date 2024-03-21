80d6b1162c462483ef95943a358f3f3fe5b45e75
Acaibird <newcoder@icloud.com>
2024-03-21 10:53:13 +0800

    feat:更新消息格式

--- meassege/meassege.go
+++ meassege/meassege.go
@@ -1,7 +1,27 @@
 package meassege
 
+import "time"
+
+type MSG interface {
+	getSender() string
+	getReceiver() string
+}
 type TextMsg struct {
-	Sender   string `json:"sender"`
-	Receiver string `json:"receiver"`
-	Content  string `json:"content"`
+	Type     string    `json:"type"`
+	Sender   string    `json:"sender"`
+	Receiver string    `json:"receiver"`
+	Content  string    `json:"content"`
+	Time     time.Time `json:"time"`
+}
+
+func (t TextMsg) GetSender() string {
+	return t.Sender
+}
+
+func (t TextMsg) GetReceiver() string {
+	return t.Receiver
+}
+
+func (t TextMsg) GetContent() string {
+	return t.Content
 }
